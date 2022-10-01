f178ea59c7adfd2c8e417870ca79f32e366ea43d
Brendon <brendon.shaw8921@gmail.com>
2022-09-30 20:33:09 -0400

    add static import simulation

---
+++ src/Chapter7/ImportTest.java
@@ -0,0 +1,9 @@
+package Chapter7;
+import static Chapter7.StaticTest.*;
+
+public class ImportTest {
+	public static void main(String[] args) {
+		System.out.println(cars + s);
+		printSeven();
+	}
+}
---
+++ src/Chapter7/StaticTest.java
@@ -0,0 +1,10 @@
+package Chapter7;
+
+public class StaticTest {
+	public static int cars = 5;
+	public static String s = "TEXT";
+	
+	public static void printSeven() {
+		System.out.println("7");
+	}
+}
